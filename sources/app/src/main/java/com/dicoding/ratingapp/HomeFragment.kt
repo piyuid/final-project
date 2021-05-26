df9deead8e3cda58e33a8e19059a1f8b830e6c29
aldiamri120 <73629504+aldiamri120@users.noreply.github.com>
2021-05-26 18:44:19 +0700

    progress search penambahan aset

--- sources/app/src/main/java/com/dicoding/ratingapp/HomeFragment.kt
+++ sources/app/src/main/java/com/dicoding/ratingapp/HomeFragment.kt
@@ -5,9 +5,17 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.LinearLayout
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.dicoding.ratingapp.home.HomeAdapter
+import com.dicoding.ratingapp.model.dummy.HomeModel
+import kotlinx.android.synthetic.main.fragment_home.*
 
 
 class HomeFragment : Fragment() {
+
+    private var publiklist : ArrayList<HomeModel> = ArrayList()
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -15,4 +23,20 @@ class HomeFragment : Fragment() {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_home, container, false)
     }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+
+        initDataDummy()
+        var adapter = HomeAdapter(publiklist,this)
+        val layoutManager: RecyclerView.LayoutManager = LinearLayoutManager(context,LinearLayoutManager.HORIZONTAL,false)
+        rcList.layoutManager = layoutManager
+        rcList.adapter = adapter
+    }
+
+    fun initDataDummy() {
+        publiklist = ArrayList()
+        publiklist.add(HomeModel("Cherry Healthy","",5f))
+        publiklist.add(HomeModel("Burger Tamayo","",4f))
+        publiklist.add(HomeModel("Bakhwan Cihuy","",4.5f))}
 }
